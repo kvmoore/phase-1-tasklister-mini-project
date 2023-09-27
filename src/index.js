a423fef4e6d8530e87c936ccaf446272525dcaaa
Kenneth <kvmoore097@gmail.com>
2023-09-27 15:18:06 -0500

    phase-1 tasklister project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+console.log('java script file has been loaded')
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    newTask(e.target.new_task_description.value)
+
+  }) 
+
 });
+
+function newTask(task) {
+
+let p = document.createElement('p')
+p.textContent = task
+document.querySelector('#list').appendChild(p)
+}
+
+
+//console.log('The form has been submitted')
+//console.log(e.target.new_task_description.value)
+
+
+/*As a user, I should be able to type a task into the input field.
+As a user, I should be able to click some form of a submit button.
+As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.*/
